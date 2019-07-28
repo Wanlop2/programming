2bdab954a5df4cac3fadc06f7139010ae519d031
Wanlop2 <52350547+Wanlop2@users.noreply.github.com>
2019-07-28 18:34:13 +0700

    Add files via upload

---
+++ Exercise4_Wanlop_p.py
@@ -0,0 +1,9 @@
+x="Foundtion English"
+y="general Business"
+z="introduction to Computer Systems"
+c="computer Progarmming"
+print("---------Your Score---------")
+print(x,59,"คะแนน")
+print(y,60,"คะแนน")
+print(z,78,"คะแนน")
+print(c,70,"คะแนน")
